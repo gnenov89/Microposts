d815a4c7c4f4f4241d25e0f57a1e18114ebc0a12
Georgi <georgi.nenov89@gmail.com>
2021-12-09 13:55:30 -0500

    Basci UI ready to go

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// const greeting = 'Hello World';
+// console.log(greeting);
+
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+
+// getData('https://jsonplaceholder.typicode.com/posts');
+
+// Common JS Module Syntax
+// const person = require('./mymodule1');
+// ES2015
+
