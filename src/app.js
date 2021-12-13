9802cb9cdeba45ad9ab1c78c3519b93118bd9d6f
Georgi <georgi.nenov89@gmail.com>
2021-12-13 16:08:34 -0500

    delete post

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 // Get post
 function getPosts(){
   http.get('http://localhost:3000/posts')
@@ -33,6 +36,24 @@ http.post('http://localhost:3000/posts', data)
 
 })
 .catch(err => console.log(err));
+
+}
+
+// Delete post
+function deletePost(e){
+  if(e.target.parentElement.classList.contains('delete')){
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+      .then(data => {
+        ui.showAlert('Post Removed', 'alert alert-success');
+        getPosts();
+        
+      })
+      .catch(err => console.log('err'));
+    }
+  }
+  e.preventDefault();
 
 }
 
